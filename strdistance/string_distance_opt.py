18e319e9e2f70c0c943a0e672f337789a0c0dc88
cutajarj2 <james.cutajar@blip.pt>
2019-06-17 09:51:15 +0100

    renaming variables to match slides

--- strdistance/string_distance_opt.py
+++ strdistance/string_distance_opt.py
@@ -2,26 +2,26 @@ class StringDistanceOpt:
     def __init__(self, str_A, str_B):
         self.str_A = str_A
         self.str_B = str_B
-        self.dist1 = [-1] * (len(str_B) + 1)
-        self.dist2 = [-1] * (len(str_B) + 1)
+        self.dist_read = [-1] * (len(str_B) + 1)
+        self.dist_write = [-1] * (len(str_B) + 1)
 
         for a in range(len(str_A) + 1):
             for b in range(len(str_B) + 1):
                 if a == 0:
-                    self.dist2[b] = b
+                    self.dist_write[b] = b
                 elif b == 0:
-                    self.dist2[b] = a
+                    self.dist_write[b] = a
                 else:
                     replace_cost = 0 if self.str_A[a - 1] == self.str_B[b - 1] else 1
-                    cost_delete = self.dist1[b] + 1
-                    cost_insert = self.dist2[b - 1] + 1
-                    cost_replace = self.dist1[b - 1] + replace_cost
-                    self.dist2[b] = min(cost_delete, cost_insert, cost_replace)
-            print(self.dist2)
-            (self.dist1, self.dist2) = (self.dist2, self.dist1)
+                    cost_delete = self.dist_read[b] + 1
+                    cost_insert = self.dist_write[b - 1] + 1
+                    cost_replace = self.dist_read[b - 1] + replace_cost
+                    self.dist_write[b] = min(cost_delete, cost_insert, cost_replace)
+            (self.dist_read, self.dist_write) = (self.dist_write, self.dist_read)
+            print(self.dist_read)
 
     def distance(self):
-        return self.dist1[len(self.str_B)]
+        return self.dist_read[len(self.str_B)]
 
 
 #dist = StringDistanceOpt("TodayIsSaturday", "TomorrowIsSunday")
