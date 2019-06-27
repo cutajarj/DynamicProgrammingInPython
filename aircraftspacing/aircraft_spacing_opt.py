573e3d1030a1caf96bf6ac932c9474283cb20710
cutajarj2 <james.cutajar@blip.pt>
2019-06-27 17:01:45 +0100

    Adding aircraft spacing solution

---
+++ aircraftspacing/aircraft_spacing_opt.py
@@ -0,0 +1,19 @@
+class AircraftSpacingOpt:
+    def __init__(self, passangers):
+        self.passangers = passangers
+        self.sub_solution_i, sub_solution_i_plus_1, sub_solution_i_plus_2 = 0, 0, 0
+        for i in range(len(passangers) - 1, -1, -1):
+            choosing_first = self.passangers[i] + sub_solution_i_plus_2
+            not_choosing_first = sub_solution_i_plus_1
+            self.sub_solution_i = max(choosing_first, not_choosing_first)
+            sub_solution_i_plus_1, sub_solution_i_plus_2 = self.sub_solution_i, sub_solution_i_plus_1
+
+    def max_passangers(self):
+        return self.sub_solution_i
+
+
+# spacing = AircraftSpacingOpt([155, 55, 2, 96, 67, 203, 3, 66, 32, 65, 29, 8, 299, 323, 77, 3, 28,
+#                              128, 19, 523, 372, 2, 3, 66, 124, 38, 34, 12, 88, 23, 74, 65, 87, 434,
+#                              14, 7, 49, 38, 27, 641, 61, 58, 14, 57, 71, 11, 82, 178, 93, 191, 4])
+spacing = AircraftSpacingOpt([155, 55, 2, 96, 67, 203, 3])
+print(spacing.max_passangers())
---
+++ aircraftspacing/aircraft_spacing_rec.py
@@ -0,0 +1,17 @@
+class AircraftSpacingRec:
+    def __init__(self, passangers):
+        self.passangers = passangers
+
+    def max_passangers(self, i):
+        if i >= len(self.passangers):
+            return 0
+        choosing_first = self.passangers[i] + self.max_passangers(i + 2)
+        not_choosing_first = self.max_passangers(i + 1)
+        return max(choosing_first, not_choosing_first)
+
+
+# spacing = AircraftSpacingRec([155, 55, 2, 96, 67, 203, 3, 66, 32, 65, 29, 8, 299, 323, 77, 3, 28,
+#                              128, 19, 523, 372, 2, 3, 66, 124, 38, 34, 12,88, 23 ,74,65, 87, 434,
+#                              14, 7, 49, 38, 27, 641, 61, 58, 14, 57, 71, 11, 82, 178, 93, 191, 4])
+spacing = AircraftSpacingRec([155, 55, 2, 96, 67, 203, 3])
+print(spacing.max_passangers(0))
