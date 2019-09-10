9d7f5f7b8ca806284818eeed2f22c8e740310825
cutajarj <james.cutajar@gmail.com>
2019-09-10 11:58:46 +0100

    Refactoring solutions to be more concise

--- exercise/full_bus_tour.py
+++ exercise/full_bus_tour.py
@@ -11,13 +11,13 @@
 # That is from the list of group bookings you can completely fill up a bus
 #
 # Assume:
-# capacity c is greater than 0
+# capacity full_cap is greater or equal to 0
 # each integer in groupSizes is greater than 0
 #
 # Examples:
-# groupSizes = {4, 13, 5, 12, 6, 1, 8},  c = 11 should return true as subset (5, 6) has a sum of 9
-# groupSizes = {1, 1, 1},  c = 3 should return true as subset (1, 1, 1) has a sum of 3
-# groupSizes = {4, 5, 6, 7},  c = 100 should return false
+# groupSizes = {4, 13, 5, 12, 6, 1, 8},  full_cap = 11 should return true as subset (5, 6) has a sum of 9
+# groupSizes = {1, 1, 1},  full_cap = 3 should return true as subset (1, 1, 1) has a sum of 3
+# groupSizes = {4, 5, 6, 7},  full_cap = 100 should return false
 
 
 class FullBusTour:
